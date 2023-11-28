754e4493a4d71d2dff7bce79b08d2548b1e6f4d3
Rebeca Nepomuceno <148262335+rebecanepom@users.noreply.github.com>
2023-11-28 16:06:41 -0300

    challenge 3

---
+++ desafio3/script.js
@@ -0,0 +1,9 @@
+function submit() {
+    let num1 = document.querySelector('div#one')
+    let num2 = document.querySelector('div#two')
+    let num3 = document.querySelector('div#three')
+    let num4 = document.querySelector('div#four')
+    let num5 = document.querySelector('div#five')
+
+    
+}
